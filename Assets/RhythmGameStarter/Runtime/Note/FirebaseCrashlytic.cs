2308fa4ab252b949ed54abce15e4b3107e1b3e66
Hong Hai Nguyen <nguyenhonghailqd1996@gmail.com>
2021-10-07 17:07:00 +0700

    no message

---
+++ Assets/RhythmGameStarter/Runtime/Note/FirebaseCrashlytic.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Crashlytics;
+
+public class FirebaseCrashlytic : MonoBehaviour
+{
+    private void Start()
+    {
+        // Crashlytics.Log();
+    }
+}
